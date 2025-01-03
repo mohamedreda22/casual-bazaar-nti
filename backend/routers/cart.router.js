77a136c91e73e81d53cc4869593638d285062ff9
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2025-01-04 00:43:21 +0200

    feat: enhance cart router with new endpoints and JWT support

--- backend/routers/cart.router.js
+++ backend/routers/cart.router.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const cartController = require("../controllers/cart.controller");
 
 router.post("/", cartController.createCart);
-router.get("/:id", cartController.getCart); 
-router.get("/user/:userId", cartController.getCartByUser);
+router.get("/", cartController.getAllCarts);
+router.get("/:id", cartController.getCart);
+router.get("/user/:userId/cart", cartController.getCartByUser);
 router.post("/:cartId/products", cartController.addToCart);
 router.put("/:cartId", cartController.updateCart);
 router.delete("/:cartId/products/:productId", cartController.removeFromCart);
+router.delete("/:cartId", cartController.clearCart);
 
 module.exports = router;
