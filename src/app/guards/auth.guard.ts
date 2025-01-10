5b28a87997befbbe35f7f3353b8392f396a618ec
mohamedreda22 <mohamed_aboreda_2020@yahoo.com>
2025-01-10 19:58:49 +0200

    feat: Update login component to improve form validation and enhance admin check logic in login process

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -6,7 +6,7 @@ export const authGuard: CanActivateFn = (route, state) => {
   // we are using the inject function to inject the AuthService and Router services
   const _authS = inject(AuthServiceService); // injecting the AuthService to get the tokenSubject value to check if the user is authenticated or not
   const _router = inject(Router); // injecting the Router to navigate to the login page if the user is not authenticated
-  if (_authS.isAuthenticated() && _authS.isAdmin()) {
+  if (/* _authS.isAuthenticated() && */ _authS.isAdmin()) {
     return true;
   } else {
     _router.navigate(['/home']); // navigating to the login page if the user is not authenticated
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,34 +1,36 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { AuthServiceService } from '../services/auth.service.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  standalone: false,
+  standalone: false, 
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrls: ['./login.component.css'], 
 })
 export class LoginComponent {
-  constructor(private _authS:AuthServiceService,private _router: Router) {}
+  constructor(private _authS: AuthServiceService, private _router: Router) {}
 
-  login(loginForm: NgForm) {
-    this._authS.login(loginForm.value).subscribe({
-      next: (res) => {
-        console.log("response: ",res);
-        if(this._authS.isAdmin()){
-        this._router.navigate(['/admin-dashboard']);}
-        else{
-          this._router.navigate(['/home']);
-        }
-      },
-      error: (err) => {
-        console.log("error: ",err);
-      },
-
-    });
-    console.log(loginForm.value);
+  login(loginForm: any): void {
+    // Accept form data from the template
+    if (loginForm.valid) {
+      this._authS.login(loginForm.value).subscribe({
+        next: (res) => {
+          console.log('response: ', res);
+          this._authS.isAdmin().subscribe((isAdmin) => {
+            if (isAdmin === true) {
+              this._router.navigate(['/admin-dashboard']);
+            } else {
+              this._router.navigate(['/home']);
+            }
+          });
+        },
+        error: (err) => {
+          console.log('error: ', err);
+        },
+      });
+    } else {
+      console.log('Form is invalid');
+    }
   }
-
-
 }
